7fd7fbb66cd2a814a6aea31817ec1fba194546e0
xstrly <ylistudy@163.com>
2023-05-18 14:13:14 +0800

    初始化项目

---
+++ TeklaOpenApiDrawing/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace TeklaOpenApiDrawing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.btn_refresh = new System.Windows.Forms.Button();
+            this.btn_Create = new System.Windows.Forms.Button();
+            this.btn_Create_GA = new System.Windows.Forms.Button();
+            this.btn_Create_component = new System.Windows.Forms.Button();
+            this.btn_Create_Parts = new System.Windows.Forms.Button();
+            this.btn_print = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn_editor = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(140, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(410, 304);
+            this.treeView1.TabIndex = 0;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(92, 25);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "打开图纸";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.Location = new System.Drawing.Point(12, 73);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(122, 25);
+            this.btn_Delete.TabIndex = 15;
+            this.btn_Delete.Text = "删除图纸";
+            this.btn_Delete.UseVisualStyleBackColor = true;
+            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
+            // 
+            // btn_refresh
+            // 
+            this.btn_refresh.Location = new System.Drawing.Point(12, 43);
+            this.btn_refresh.Name = "btn_refresh";
+            this.btn_refresh.Size = new System.Drawing.Size(122, 25);
+            this.btn_refresh.TabIndex = 14;
+            this.btn_refresh.Text = "刷新列表";
+            this.btn_refresh.UseVisualStyleBackColor = true;
+            this.btn_refresh.Click += new System.EventHandler(this.btn_refresh_Click);
+            // 
+            // btn_Create
+            // 
+            this.btn_Create.Location = new System.Drawing.Point(12, 197);
+            this.btn_Create.Name = "btn_Create";
+            this.btn_Create.Size = new System.Drawing.Size(122, 25);
+            this.btn_Create.TabIndex = 17;
+            this.btn_Create.Text = "创建";
+            this.btn_Create.UseVisualStyleBackColor = true;
+            this.btn_Create.Click += new System.EventHandler(this.btn_Create_Click);
+            // 
+            // btn_Create_GA
+            // 
+            this.btn_Create_GA.Location = new System.Drawing.Point(12, 166);
+            this.btn_Create_GA.Name = "btn_Create_GA";
+            this.btn_Create_GA.Size = new System.Drawing.Size(122, 25);
+            this.btn_Create_GA.TabIndex = 18;
+            this.btn_Create_GA.Text = "创建GA图";
+            this.btn_Create_GA.UseVisualStyleBackColor = true;
+            this.btn_Create_GA.Click += new System.EventHandler(this.btn_Create_GA_Click);
+            // 
+            // btn_Create_component
+            // 
+            this.btn_Create_component.Location = new System.Drawing.Point(12, 104);
+            this.btn_Create_component.Name = "btn_Create_component";
+            this.btn_Create_component.Size = new System.Drawing.Size(122, 25);
+            this.btn_Create_component.TabIndex = 19;
+            this.btn_Create_component.Text = "创建构件图";
+            this.btn_Create_component.UseVisualStyleBackColor = true;
+            this.btn_Create_component.Click += new System.EventHandler(this.btn_Create_component_Click);
+            // 
+            // btn_Create_Parts
+            // 
+            this.btn_Create_Parts.Location = new System.Drawing.Point(12, 135);
+            this.btn_Create_Parts.Name = "btn_Create_Parts";
+            this.btn_Create_Parts.Size = new System.Drawing.Size(122, 25);
+            this.btn_Create_Parts.TabIndex = 16;
+            this.btn_Create_Parts.Text = "创建零件图";
+            this.btn_Create_Parts.UseVisualStyleBackColor = true;
+            this.btn_Create_Parts.Click += new System.EventHandler(this.btn_Create_Parts_Click);
+            // 
+            // btn_print
+            // 
+            this.btn_print.Location = new System.Drawing.Point(13, 229);
+            this.btn_print.Name = "btn_print";
+            this.btn_print.Size = new System.Drawing.Size(121, 25);
+            this.btn_print.TabIndex = 20;
+            this.btn_print.Text = "打印";
+            this.btn_print.UseVisualStyleBackColor = true;
+            this.btn_print.Click += new System.EventHandler(this.btn_print_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 291);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 25);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "关闭";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_editor
+            // 
+            this.btn_editor.Location = new System.Drawing.Point(12, 260);
+            this.btn_editor.Name = "btn_editor";
+            this.btn_editor.Size = new System.Drawing.Size(121, 25);
+            this.btn_editor.TabIndex = 21;
+            this.btn_editor.Text = "编辑";
+            this.btn_editor.UseVisualStyleBackColor = true;
+            this.btn_editor.Click += new System.EventHandler(this.btn_editor_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 324);
+            this.Controls.Add(this.btn_editor);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_print);
+            this.Controls.Add(this.btn_Create);
+            this.Controls.Add(this.btn_Create_GA);
+            this.Controls.Add(this.btn_Create_component);
+            this.Controls.Add(this.btn_Create_Parts);
+            this.Controls.Add(this.btn_Delete);
+            this.Controls.Add(this.btn_refresh);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.treeView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Tekla";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.Button btn_refresh;
+        private System.Windows.Forms.Button btn_Create;
+        private System.Windows.Forms.Button btn_Create_GA;
+        private System.Windows.Forms.Button btn_Create_component;
+        private System.Windows.Forms.Button btn_Create_Parts;
+        private System.Windows.Forms.Button btn_print;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_editor;
+    }
+}
+
---
+++ TeklaOpenApiDrawing/Form1.cs
@@ -0,0 +1,394 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tekla.Structures.Drawing;
+using Tekla.Structures.Model;
+using Tekla.Structures;
+
+namespace TeklaOpenApiDrawing
+{
+    public partial class Form1 : Form
+    {
+        private static Model _model => new Model();
+        private static DrawingHandler drawingHandler => new DrawingHandler();
+
+        public Form1()
+        {
+            if (_model.GetConnectionStatus())
+                InitializeComponent();
+            InitTreeData();
+        }
+
+        /// <summary>
+        /// 获取图纸数据
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        private void InitTreeData()
+        {
+            if (drawingHandler.GetConnectionStatus() && _model.GetConnectionStatus())
+            {
+                treeView1?.Nodes.Clear();
+                DrawingEnumerator enumerator = drawingHandler.GetDrawings();
+                while (enumerator.MoveNext())
+                {
+                    Drawing drawing = enumerator.Current as Drawing;
+                    TreeNode node = new TreeNode();
+                    node.Tag = drawing;
+                    node.Text = drawing.Name + " " + GetDrawingTypeCharacter(drawing);
+                    treeView1.Nodes.Add(node);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 获取图纸的Type类型
+        /// </summary>
+        /// <param name="DrawingInstance"></param>
+        /// <returns></returns>
+        private string GetDrawingTypeCharacter(Drawing DrawingInstance)
+        {
+            string Result = "U"; // Unknown drawing
+
+            if (DrawingInstance is GADrawing)
+            {
+                Result = "G";
+            }
+            else if (DrawingInstance is AssemblyDrawing)
+            {
+                Result = "A";
+            }
+            else if (DrawingInstance is CastUnitDrawing)
+            {
+                Result = "C";
+            }
+            else if (DrawingInstance is MultiDrawing)
+            {
+                Result = "M";
+            }
+            else if (DrawingInstance is SinglePartDrawing)
+            {
+                Result = "W";
+            }
+
+            return Result;
+        }
+
+        /// <summary>
+        /// 刷新图纸列表
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_refresh_Click(object sender, EventArgs e)
+        {
+            InitTreeData();
+        }
+
+        private void DeleteDrawing()
+        {
+            if (treeView1.SelectedNode == null)
+            {
+                MessageBox.Show("当前没有选中图纸！");
+                return;
+            }
+            if (DialogResult.Yes == MessageBox.Show("是否要删除已选中的图纸？", "提示", MessageBoxButtons.YesNoCancel))
+            {
+                if (drawingHandler.GetActiveDrawing() != null)
+                {
+                    MessageBox.Show("请关闭图纸在进行删除");
+                    return;
+                }
+                if (!drawingHandler.GetConnectionStatus())
+                    return;
+                if (treeView1.Nodes == null || treeView1.SelectedNode == null)
+                    MessageBox.Show("当前没有图纸！");
+                if (treeView1.Nodes != null && treeView1.SelectedNode?.Tag is DatabaseObject)
+                {
+                    if ((treeView1.SelectedNode.Tag as DatabaseObject).Delete())
+                    {
+                        treeView1.SelectedNode.Remove();
+                        InitTreeData();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 删除图纸
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+            DeleteDrawing();
+        }
+
+        private void CreateAssemblyDrarwing()
+        {
+            try
+            {
+                //获取选择的模型
+                ModelObjectEnumerator modelObject = new Tekla.Structures.Model.UI.ModelObjectSelector().GetSelectedObjects();
+                while (modelObject.MoveNext())
+                {
+                    var obj = modelObject.Current as Tekla.Structures.Model.Part;
+                    if (obj == null) return;
+                    Tekla.Structures.ModelInternal.Operation.dotStartAction("FullNumbering", (string)null);
+                    //获取零件的identifier
+                    Identifier identifier = obj.GetAssembly().Identifier;
+                    AssemblyDrawing assembly = new AssemblyDrawing(identifier);
+                    assembly.Insert();
+                    if (checkBox1.Checked)
+                        drawingHandler.SetActiveDrawing(assembly);
+                    else
+                        drawingHandler.SetActiveDrawing(assembly, false);
+                    drawingHandler.CloseActiveDrawing();
+                    if (checkBox1.Checked && assembly != null)
+                        drawingHandler.SetActiveDrawing(assembly);
+                    InitTreeData();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// 创建构件图
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Create_component_Click(object sender, EventArgs e)
+        {
+            CreateAssemblyDrarwing();
+        }
+
+        /// <summary>
+        /// 创建GA图
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Create_GA_Click(object sender, EventArgs e)
+        {
+            TransformationPlane current = _model.GetWorkPlaneHandler().GetCurrentTransformationPlane();
+            _model.GetWorkPlaneHandler().SetCurrentTransformationPlane(new TransformationPlane());  // We use global transformation
+            try
+            {
+                GADrawing gADrawing = new GADrawing("GA Drawing", "standard");
+                //如果大小设置为0，边框不显示,设置尺寸为0的话，打印图纸会报错，显示文件损坏
+                //gADrawing.Layout.SheetSize = new Tekla.Structures.Drawing.Size();
+                gADrawing.PlaceViews();
+                gADrawing.Insert();
+                _model.GetWorkPlaneHandler().SetCurrentTransformationPlane(current); // return original transformation
+                if (checkBox1.Checked)
+                {
+                    drawingHandler.SetActiveDrawing(gADrawing);
+                }
+                else
+                {
+                    drawingHandler.SetActiveDrawing(gADrawing, false);
+                }
+                //需要先使用CloseActiceDrawing来关闭，不然虽然能生成图纸但是打不开
+                drawingHandler.CloseActiveDrawing(true);
+                //在判断一次是否要打开图纸
+                if (drawingHandler.GetConnectionStatus() && checkBox1.Checked)
+                {
+                    drawingHandler.SetActiveDrawing(gADrawing);
+                }
+                InitTreeData();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// 创建零件图
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Create_Parts_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ModelObjectEnumerator modelObject = new Tekla.Structures.Model.UI.ModelObjectSelector().GetSelectedObjects();
+                while (modelObject.MoveNext())
+                {
+                    var obj = modelObject.Current as Tekla.Structures.Model.Part;
+                    if (obj == null) return;
+                    Identifier identifier = obj.Identifier;
+                    SinglePartDrawing singlePart = new SinglePartDrawing(identifier, "standard");
+                    singlePart.Insert();
+                    if (checkBox1.Checked)
+                        drawingHandler.SetActiveDrawing(singlePart);
+                    else
+                        drawingHandler.SetActiveDrawing(singlePart, false);
+                    drawingHandler.CloseActiveDrawing();
+                    if (checkBox1.Checked && singlePart != null)
+                        drawingHandler.SetActiveDrawing(singlePart);
+                    InitTreeData();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// 创建模型
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Create_Click(object sender, EventArgs e)
+        {
+            var cp1 = new ContourPlate();
+            var cp2 = new ContourPlate();
+
+            Contour contour1 = new Contour();
+            contour1.AddContourPoint(new ContourPoint(new Tekla.Structures.Geometry3d.Point(2000, 0, 0), null));
+            contour1.AddContourPoint(new ContourPoint(new Tekla.Structures.Geometry3d.Point(3000, 0, 0), null));
+            contour1.AddContourPoint(new ContourPoint(new Tekla.Structures.Geometry3d.Point(3000, 2000, 0), null));
+            contour1.AddContourPoint(new ContourPoint(new Tekla.Structures.Geometry3d.Point(2000, 2000, 0), null));
+
+            Contour contour2 = new Contour();
+            contour2.AddContourPoint(new ContourPoint(new Tekla.Structures.Geometry3d.Point(3000, 0, 0), null));
+            contour2.AddContourPoint(new ContourPoint(new Tekla.Structures.Geometry3d.Point(3000, 2000, 0), null));
+            contour2.AddContourPoint(new ContourPoint(new Tekla.Structures.Geometry3d.Point(4000, 2000, 0), null));
+            contour2.AddContourPoint(new ContourPoint(new Tekla.Structures.Geometry3d.Point(4000, 0, 0), null));
+
+            cp1.Contour = contour1;
+            cp2.Contour = contour2;
+
+            cp1.Profile.ProfileString = cp2.Profile.ProfileString = "PL100";
+
+            cp1.Insert();
+            cp2.Insert();
+
+            var weld = new Tekla.Structures.Model.Weld();
+            weld.MainObject = cp1;
+            weld.SecondaryObject = cp2;
+            weld.SizeAbove = 10;
+            weld.Insert();
+
+            double weldLength = -1;
+
+            weld.GetReportProperty("LENGTH", ref weldLength);
+
+            Tekla.Structures.ModelInternal.Operation.dotStartAction("FullNumbering", (string)null);
+
+            _model.CommitChanges();
+        }
+
+        /// <summary>
+        /// 打印图纸
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <exception cref="Exception"></exception>
+        private void btn_print_Click(object sender, EventArgs e)
+        {
+            //获取当前的图纸
+            var drawings = drawingHandler.GetActiveDrawing();
+            //如果不是ga图、零件图、构件图直接return掉
+            if (!(drawings is GADrawing || drawings is AssemblyDrawing || drawings is SinglePartDrawing)) return;
+            DPMPrinterAttributes printAttributes = new DPMPrinterAttributes();
+            printAttributes.ColorMode = DotPrintColor.BlackAndWhite;
+            printAttributes.NumberOfCopies = 1;
+            printAttributes.OpenFileWhenFinished = false;
+            printAttributes.Orientation = DotPrintOrientationType.Landscape;
+            printAttributes.OutputType = DotPrintOutputType.PDF;
+            printAttributes.PaperSize = DotPrintPaperSize.A4;
+            printAttributes.PrinterName = "Microsoft Print to PDF"; // Must use local PDF printer name
+            printAttributes.PrintToMultipleSheet = DotPrintToMultipleSheet.Off;
+            printAttributes.ScaleFactor = 1.0;
+            printAttributes.ScalingMethod = DotPrintScalingType.Auto;
+            //保存对话框
+            SaveFileDialog folderDlg = new SaveFileDialog();
+            //设置保存的格式
+            folderDlg.Filter = "PDF格式(*.pdf)|*.pdf";
+            //设置名字和位置
+            if (folderDlg.ShowDialog() == DialogResult.OK)
+            {
+                //获取选择的路径
+                var inputPath = folderDlg.FileName;
+                printAttributes.OutputFileName = inputPath;
+            }
+            var result = drawingHandler.PrintDrawing(drawings, printAttributes);
+            if (!result)
+            {
+                throw new Exception("打印失败！");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            drawingHandler.CloseActiveDrawing(true);
+        }
+
+        /// <summary>
+        /// 编辑图纸
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_editor_Click(object sender, EventArgs e)
+        {
+            if (drawingHandler.GetConnectionStatus())
+            {
+                var drawings = drawingHandler.GetActiveDrawing() as Drawing;
+                if (!(drawings is GADrawing)) return;
+                //获取图纸的工作表
+                ContainerView Sheet = drawings.GetSheet();
+                for (int i = 0; i < 400; i += 100)
+                {
+                    if (i == 0)
+                    {
+                        DrawLine(0, 0, 300, 0, drawings, Sheet);
+                    }
+                    else if (i == 100)
+                    {
+                        DrawLine(300, 0, 300, 100, drawings, Sheet);
+                    }
+                    else if (i == 200)
+                    {
+                        DrawLine(0, 0, 0, 100, drawings, Sheet);
+                    }
+                    else
+                    {
+                        DrawLine(0, 100, 300, 100, drawings, Sheet);
+                    }
+                }
+                drawingHandler.SaveActiveDrawing();
+            }
+        }
+
+        /// <summary>
+        /// drawing--画线
+        /// </summary>
+        /// <param name="X1">开始的X</param>
+        /// <param name="Y1">开始的Y</param>
+        /// <param name="X2">结束的X</param>
+        /// <param name="Y2">结束的Y</param>
+        /// <param name="drawings">当前图纸</param>
+        /// <param name="Sheet">当前视图</param>
+        private void DrawLine(double X1, double Y1, double X2, double Y2, Drawing drawings, ContainerView Sheet)
+        {
+            Tekla.Structures.Geometry3d.Point point = new Tekla.Structures.Geometry3d.Point();
+            Tekla.Structures.Geometry3d.Point point1 = new Tekla.Structures.Geometry3d.Point();
+            point.X = X1;
+            point.Y = Y1;
+            point1.X = X2;
+            point1.Y = Y2;
+            Tekla.Structures.Drawing.Line line = new Tekla.Structures.Drawing.Line(Sheet, point, point1, 0.4);
+            line.Insert();
+            drawings.CommitChanges();
+        }
+    }
+}
