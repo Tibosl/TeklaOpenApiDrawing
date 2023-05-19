b3830c7f2b56f92c62d4378760fc39871638a248
xstrly <ylistudy@163.com>
2023-05-19 16:20:43 +0800

    修改图标

--- TeklaOpenApiDrawing/Form1.Designer.cs
+++ TeklaOpenApiDrawing/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.btn_Delete = new System.Windows.Forms.Button();
@@ -164,6 +165,7 @@
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.treeView1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
